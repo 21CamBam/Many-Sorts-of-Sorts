97612d7b3855a1bae9b226ef5b1a9bdd9058e3f6
Cammi Smith <cammi.smith@email.wsu.edu>
2014-07-18 17:36:07 -0700

    Create main.h

---
+++ main.h
@@ -0,0 +1,10 @@
+// The following code is the base code given to me by my professor to run my program
+// Please note that it is not mine
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include "ArraySorter.h"
+
+int* GenArr(int count);
+void EvanSort(int* arr, int n);
